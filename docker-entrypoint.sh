d08438c4a4c3f518fbf8a46b423303bb11df0178
Felix Khachatrian <felix.khachatrian@gmail.com>
2020-06-05 12:06:30 +0400

    Node 12

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,8 +4,8 @@ rm -rf /app/*
 mkdir -p /app/_next
 mkdir -p /app/static
 
-rsync -a /var/www/app/src/.next/static /app/_next
-rsync -a /var/www/app/src/static/assets /app/static
+rsync -a /var/www/app/.next/static /app/_next
+rsync -a /var/www/app/static/assets /app/static
 
 # Learn more: https://nextjs.org/telemetry
 npx next telemetry disable
